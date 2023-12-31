842b67ad62fb18264a95594177b8ddf6da82f59b
Bharathi V <128215224+Bharathiv1403@users.noreply.github.com>
2023-12-31 19:16:47 +0530

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,17 @@
+# a='how are you my dear frds'
+# print(a.split())
+
+bike = input("Enter Your Bike : ")
+ 
+if bike == 'Hero':
+    print("bike is Hero")
+ 
+elif bike == "Suzuki":
+    print("bike is Suzuki")
+ 
+elif bike == "Yamaha":
+    print("bike is Yamaha")
+ 
+else:
+    print("Please choose correct answer")
+
